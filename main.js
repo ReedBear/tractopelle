eb37ed6b3f1df0dc47d9e7d16cc9b29edd8f4321
Julien Seren-Rosso <j.serenrosso@gmail.com>
2012-06-19 20:14:06 +0200

    Added initial files.

---
+++ main.js
@@ -0,0 +1,25 @@
+var application_root = __dirname,
+  express = require('express'),
+  path = require('path'),
+  less = require('less'),
+  ejs = require('ejs'),
+  routesHandler = require('./controllers/RoutesHandler');
+
+var app = express.createServer();
+
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(application_root, 'public')));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+
+// Routes
+app.get('/', function (req, res) {
+  routesHandler.index(req, res);
+});
+
+
+app.listen(4242);
