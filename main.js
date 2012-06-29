a1fd685c0ebc6c5379fb98685130425e5d5902af
Julien Seren-Rosso <j.serenrosso@gmail.com>
2012-06-29 10:34:27 -0400

    Added environments and LESS compiling.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var application_root = __dirname,
   express = require('express'),
+  fs = require("fs"),
   path = require('path'),
   less = require('less'),
   ejs = require('ejs'),
@@ -7,15 +8,39 @@ var application_root = __dirname,
 
 var app = express.createServer();
 
+// App Configuration
 app.configure(function () {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(application_root, 'public')));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
   app.set('view engine', 'ejs');
 });
 
+
+app.configure('development', function() {
+  app.use(express.static(path.join(application_root, 'public')));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function() {
+  var oneYear = 31557600000;
+  app.use(express.static(path.join(application_root, 'public'), { maxAge: oneYear }));
+  app.use(express.errorHandler());
+});
+
+
+// Less
+app.get("*.less", function(req, res) {
+  var path = __dirname + req.url;
+  fs.readFile(path, "utf8", function(err, data) {
+    if (err) throw err;
+    less.render(data, function(err, css) {
+      if (err) throw err;
+      res.header("Content-type", "text/css");
+      res.send(css);
+    });
+  });
+});
 
 // Routes
 app.get('/', function (req, res) {
